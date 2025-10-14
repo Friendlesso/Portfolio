3460c2891d44ae5f5fc0a4785934773bddfbe6de
Mihailo <mihailoless@gmail.com>
2025-10-15 00:19:27 +0200

    Windows Fix

--- src/pages/GitHub/GitHubWindow.tsx
+++ src/pages/GitHub/GitHubWindow.tsx
@@ -14,7 +14,7 @@ export function GitHubWindow({ item, onClose }: GitHubProps) {
   if (!item) return
   return (
     <div className={`dual-border-folder p-1 bg-[var(--folder-background)] flex flex-col transition-all duration-350
-      ${isMaximized ? " w-[100vw] h-[100vh] pb-[3.5rem] " : " h-[80vh]  min-w-[450px] max-w-[1050px] " }
+      ${isMaximized ? " w-[100vw] h-[100dvh] pb-[3.5rem] " : " h-[80vh]  min-w-[450px] max-w-[1050px] " }
     `}>
       <WindowHeader label={item.label} icon={item.icon} bgColor={item.headerColor} onClose={onClose} isMaximized={isMaximized} onMaximize={toggleMaximized} disableMaximize={isSmallScreen} />
       <div className="bg-[var(--github-background)] min-[1700px]:flex flex-1 px-4 sm:px-6 lg:px-12 overflow-y-auto text-white">
