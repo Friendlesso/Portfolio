1983f4d536b7894f75ed0534e2819ddf3509f264
Mihailo <mihailoless@gmail.com>
2025-10-14 23:07:06 +0200

    Fixing 2

--- src/pages/ResumeWindow/ResumeWindow.tsx
+++ src/pages/ResumeWindow/ResumeWindow.tsx
@@ -18,7 +18,7 @@ export function ResumeWindow({ item, onClose }: ResumePageProps) {
 
   return (
     <div className={`dual-border-folder p-1 bg-[var(--folder-background)] transition-all duration-350
-      ${isMaximized ? "h-[100dvh] w-[100vw] pb-[3.5rem]" : "h-fit w-fit min-w-[500px] max-w-[950px]"}
+      ${isMaximized ? "h-[100dvh] w-[100vw]" : "h-fit w-fit min-w-[500px] max-w-[950px]"}
     `}>
       <WindowHeader
         label={item.label}
@@ -29,16 +29,19 @@ export function ResumeWindow({ item, onClose }: ResumePageProps) {
         onMaximize={toggleMaximized}
         disableMaximize={isSmallScreen}
       />
-      <div className="bg-[var(--folder-box-color)] p-1">
-        <a href={resumePdf} download="Mihailo-Djurovic.pdf" className="dual-border mr-2 p-1 text-lg">Download</a>
-        <a href={resumePdf} target="_blank" rel="noopener noreferrer" className="dual-border p-1 text-lg">Open in a new tab</a>
-      </div>
-      <div className="bg-black flex justify-center items-center h-[calc(100%-3.5rem)]">
-        <div className={`flex flex-col max-h-[75vh] overflow-y-scroll ${isMaximized ? "max-h-[80vh]" : ""}`}>
-          <img src={resumePageOne} loading="lazy" className="lg:max-h-[75vh] max-h-[85vh] max-w-full h-auto w-auto object-contain mb-2" alt="Resume Picture" />
-          <img src={resumePageTwo} loading="lazy" className="lg:max-h-[75vh] max-h-[85vh] max-w-full h-auto w-auto object-contain" alt="Resume Picture"  />
+      <div className="flex-1 flex flex-col">
+        <div className="bg-[var(--folder-box-color)] p-1">
+          <a href={resumePdf} download="Mihailo-Djurovic.pdf" className="dual-border mr-2 p-1 text-lg">Download</a>
+          <a href={resumePdf} target="_blank" rel="noopener noreferrer" className="dual-border p-1 text-lg">Open in a new tab</a>
+        </div>
+        <div className="bg-black flex flex-1 flex-col justify-center items-center h-[100vh]">
+          <div className={`flex flex-col max-h-[75vh] overflow-y-scroll ${isMaximized ? "max-h-[90vh]" : ""}`}>
+            <img src={resumePageOne} loading="lazy" className="lg:max-h-[75vh] max-h-[85vh] max-w-full h-auto w-auto object-contain mb-2" alt="Resume Picture" />
+            <img src={resumePageTwo} loading="lazy" className="lg:max-h-[75vh] max-h-[85vh] max-w-full h-auto w-auto object-contain" alt="Resume Picture" />
+          </div>
         </div>
       </div>
+
     </div>
   )
 }
