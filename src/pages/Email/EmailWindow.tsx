958f42ccda64c3d59df3c9eda9ce7de1c1016c23
Mihailo <mihailoless@gmail.com>
2025-10-15 00:11:18 +0200

    Email Fix

--- src/pages/Email/EmailWindow.tsx
+++ src/pages/Email/EmailWindow.tsx
@@ -12,19 +12,21 @@ export function EmailWindow({ item, onClose }: EmailWindowProps) {
 
   if (!item) return;
   return (
-      <div className={`dual-border-folder p-1 bg-[var(--folder-background)] flex flex-col transition-all duration-350
-        ${isMaximized ? 'w-[100vw] h-[100vh] pb-[3.75rem]' : 'h-[50vh] w-[30vw] min-w-[350px] max-w-[950px]'}
+    <div className={`dual-border-folder p-1 bg-[var(--folder-background)] flex flex-col transition-all duration-350
+        ${isMaximized ? 'w-[100vw] h-[100dvh] pb-[3.75rem]' : 'h-[50vh] w-[30vw] min-w-[350px] max-w-[950px]'}
       `}>
-        <WindowHeader
-          label={item.label}
-          icon={item.icon}
-          bgColor={item.headerColor}
-          onClose={onClose}
-          isMaximized={isMaximized}
-          onMaximize={toggleMaximized}
-          disableMaximize={isSmallScreen}
-        />
+      <WindowHeader
+        label={item.label}
+        icon={item.icon}
+        bgColor={item.headerColor}
+        onClose={onClose}
+        isMaximized={isMaximized}
+        onMaximize={toggleMaximized}
+        disableMaximize={isSmallScreen}
+      />
+      <section className="flex flex-1">
         <EmailForm />
-      </div>
+      </section>
+    </div>
   )
 }
