e5727325cca44eed848128629c3dbbb32bf91965
Mihailo <mihailoless@gmail.com>
2025-10-16 14:37:55 +0200

    Double click fixed

--- src/pages/desktop/DesktopPage.tsx
+++ src/pages/desktop/DesktopPage.tsx
@@ -101,7 +101,6 @@ export function DesktopPage() {
                 } else {
                   setSelectedIndex(index)
                 }
-
               }}
 
               onDoubleClick={() => {
